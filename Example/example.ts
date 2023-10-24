cc86308f4caa70c9ce0e8ee069d71fc8b431a0a9
canove <cassio@economicros.com.br>
2023-10-24 16:09:11 -0300

    chore: remove unused code

--- Example/example.ts
+++ Example/example.ts
@@ -1,8 +1,17 @@
 import { Boom } from '@hapi/boom'
-import makeWASocket, { AnyMessageContent, delay, DisconnectReason, fetchLatestBaileysVersion, makeCacheableSignalKeyStore, makeInMemoryStore, MessageRetryMap, useMultiFileAuthState } from '../src'
+import makeWASocket, {
+  AnyMessageContent,
+  delay,
+  DisconnectReason,
+  fetchLatestBaileysVersion,
+  makeCacheableSignalKeyStore,
+  makeInMemoryStore,
+  MessageRetryMap,
+  useMultiFileAuthState
+} from '../src'
 import MAIN_LOGGER from '../src/Utils/logger'
 
-const logger = MAIN_LOGGER.child({ })
+const logger = MAIN_LOGGER.child({})
 logger.level = 'trace'
 
 const useStore = !process.argv.includes('--no-store')
@@ -10,7 +19,7 @@ const doReplies = !process.argv.includes('--no-reply')
 
 // external map to store retry counts of messages when decryption/encryption fails
 // keep this out of the socket itself, so as to prevent a message decryption/encryption loop across socket restarts
-const msgRetryCounterMap: MessageRetryMap = { }
+const msgRetryCounterMap: MessageRetryMap = {}
 
 // the store maintains the data of the WA connection in memory
 // can be written out to a file & read from it
@@ -18,159 +27,161 @@ const store = useStore ? makeInMemoryStore({ logger }) : undefined
 store?.readFromFile('./baileys_store_multi.json')
 // save every 10s
 setInterval(() => {
-	store?.writeToFile('./baileys_store_multi.json')
+  store?.writeToFile('./baileys_store_multi.json')
 }, 10_000)
 
 // start a connection
-const startSock = async() => {
-	const { state, saveCreds } = await useMultiFileAuthState('baileys_auth_info')
-	// fetch latest version of WA Web
-	const { version, isLatest } = await fetchLatestBaileysVersion()
-	console.log(`using WA v${version.join('.')}, isLatest: ${isLatest}`)
+const startSock = async () => {
+  const { state, saveCreds } = await useMultiFileAuthState('baileys_auth_info')
+  // fetch latest version of WA Web
+  const { version, isLatest } = await fetchLatestBaileysVersion()
+  console.log(`using WA v${version.join('.')}, isLatest: ${isLatest}`)
 
-	const sock = makeWASocket({
-		version: [2, 2329, 9],
-		logger,
-		printQRInTerminal: true,
-		auth: {
-			creds: state.creds,
-			/** caching makes the store faster to send/recv messages */
-			keys: makeCacheableSignalKeyStore(state.keys, logger),
-		},
-		msgRetryCounterMap,
-		generateHighQualityLinkPreview: true,
+  const sock = makeWASocket({
+    version: [2, 2329, 9],
+    logger,
+    printQRInTerminal: true,
+    auth: {
+      creds: state.creds,
+      /** caching makes the store faster to send/recv messages */
+      keys: makeCacheableSignalKeyStore(state.keys, logger)
+    },
+    msgRetryCounterMap,
+    generateHighQualityLinkPreview: true,
     shouldSyncHistoryMessage: () => false,
     syncFullHistory: false,
-		// implement to handle retries
-		getMessage: async key => {
-			if(store) {
-				const msg = await store.loadMessage(key.remoteJid!, key.id!)
-				return msg?.message || undefined
-			}
+    // implement to handle retries
+    getMessage: async key => {
+      if (store) {
+        const msg = await store.loadMessage(key.remoteJid!, key.id!)
+        return msg?.message || undefined
+      }
 
-			// only if store is present
-			return {
-				conversation: 'hello'
-			}
-		}
-	})
+      // only if store is present
+      return {
+        conversation: 'hello'
+      }
+    }
+  })
 
+  store?.bind(sock.ev)
 
+  const sendMessageWTyping = async (msg: AnyMessageContent, jid: string) => {
+    await sock.presenceSubscribe(jid)
+    await delay(500)
 
+    await sock.sendPresenceUpdate('composing', jid)
+    await delay(2000)
 
-	store?.bind(sock.ev)
+    await sock.sendPresenceUpdate('paused', jid)
 
-	const sendMessageWTyping = async(msg: AnyMessageContent, jid: string) => {
-		await sock.presenceSubscribe(jid)
-		await delay(500)
+    await sock.sendMessage(jid, msg)
+  }
 
-		await sock.sendPresenceUpdate('composing', jid)
-		await delay(2000)
-
-		await sock.sendPresenceUpdate('paused', jid)
-
-		await sock.sendMessage(jid, msg)
-	}
-
-	// the process function lets you process all events that just occurred
-	// efficiently in a batch
-	sock.ev.process(
-		// events is a map for event name => event data
-		async(events) => {
-			// something about the connection changed
-			// maybe it closed, or we received all offline message or connection opened
-			if(events['connection.update']) {
-				const update = events['connection.update']
-				const { connection, lastDisconnect } = update
-				if(connection === 'close') {
-					// reconnect if not logged out
-					if((lastDisconnect?.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut) {
-						startSock()
-					} else {
-						console.log('Connection closed. You are logged out.')
-					}
-				}
-
-				console.log('connection update', update)
-
-        if (update.qr !== undefined && !sock.authState.creds.registered) {
-  
-          const code = await sock.requestPairingCode("551333074319")
-        
-          console.log("Paring code", code)
+  // the process function lets you process all events that just occurred
+  // efficiently in a batch
+  sock.ev.process(
+    // events is a map for event name => event data
+    async events => {
+      // something about the connection changed
+      // maybe it closed, or we received all offline message or connection opened
+      if (events['connection.update']) {
+        const update = events['connection.update']
+        const { connection, lastDisconnect } = update
+        if (connection === 'close') {
+          // reconnect if not logged out
+          if (
+            (lastDisconnect?.error as Boom)?.output?.statusCode !==
+            DisconnectReason.loggedOut
+          ) {
+            startSock()
+          } else {
+            console.log('Connection closed. You are logged out.')
+          }
         }
-			}
 
-			// credentials updated -- save them
-			if(events['creds.update']) {
-				await saveCreds()
-			}
+        console.log('connection update', update)
+      }
 
-			if(events.call) {
-				console.log('recv call event', events.call)
-			}
+      // credentials updated -- save them
+      if (events['creds.update']) {
+        await saveCreds()
+      }
 
-			// history received
-			if(events['messaging-history.set']) {
-				const { chats, contacts, messages, isLatest } = events['messaging-history.set']
-				console.log(`recv ${chats.length} chats, ${contacts.length} contacts, ${messages.length} msgs (is latest: ${isLatest})`)
-			}
+      if (events.call) {
+        console.log('recv call event', events.call)
+      }
 
-			// received a new message
-			if(events['messages.upsert']) {
-				const upsert = events['messages.upsert']
-				console.log('recv messages ', JSON.stringify(upsert, undefined, 2))
+      // history received
+      if (events['messaging-history.set']) {
+        const { chats, contacts, messages, isLatest } =
+          events['messaging-history.set']
+        console.log(
+          `recv ${chats.length} chats, ${contacts.length} contacts, ${messages.length} msgs (is latest: ${isLatest})`
+        )
+      }
 
-				if(upsert.type === 'notify') {
-					for(const msg of upsert.messages) {
-						if(!msg.key.fromMe && doReplies) {
-							console.log('replying to', msg.key.remoteJid)
-							await sock!.readMessages([msg.key])
-							await sendMessageWTyping({ text: 'Hello there!' }, msg.key.remoteJid!)
-						}
-					}
-				}
-			}
+      // received a new message
+      if (events['messages.upsert']) {
+        const upsert = events['messages.upsert']
+        console.log('recv messages ', JSON.stringify(upsert, undefined, 2))
 
-			// messages updated like status delivered, message deleted etc.
-			if(events['messages.update']) {
-				console.log(events['messages.update'])
-			}
+        if (upsert.type === 'notify') {
+          for (const msg of upsert.messages) {
+            if (!msg.key.fromMe && doReplies) {
+              console.log('replying to', msg.key.remoteJid)
+              await sock!.readMessages([msg.key])
+              await sendMessageWTyping(
+                { text: 'Hello there!' },
+                msg.key.remoteJid!
+              )
+            }
+          }
+        }
+      }
 
-			if(events['message-receipt.update']) {
-				console.log(events['message-receipt.update'])
-			}
+      // messages updated like status delivered, message deleted etc.
+      if (events['messages.update']) {
+        console.log(events['messages.update'])
+      }
 
-			if(events['messages.reaction']) {
-				console.log(events['messages.reaction'])
-			}
+      if (events['message-receipt.update']) {
+        console.log(events['message-receipt.update'])
+      }
 
-			if(events['presence.update']) {
-				console.log(events['presence.update'])
-			}
+      if (events['messages.reaction']) {
+        console.log(events['messages.reaction'])
+      }
 
-			if(events['chats.update']) {
-				console.log(events['chats.update'])
-			}
+      if (events['presence.update']) {
+        console.log(events['presence.update'])
+      }
 
-			if(events['contacts.update']) {
-				for(const contact of events['contacts.update']) {
-					if(typeof contact.imgUrl !== 'undefined') {
-						const newUrl = contact.imgUrl === null ? null : await sock!.profilePictureUrl(contact.id!)
-						console.log(
-							`contact ${contact.id} has a new profile pic: ${newUrl}`,
-						)
-					}
-				}
-			}
+      if (events['chats.update']) {
+        console.log(events['chats.update'])
+      }
 
-			if(events['chats.delete']) {
-				console.log('chats deleted ', events['chats.delete'])
-			}
-		}
-	)
+      if (events['contacts.update']) {
+        for (const contact of events['contacts.update']) {
+          if (typeof contact.imgUrl !== 'undefined') {
+            const newUrl =
+              contact.imgUrl === null
+                ? null
+                : await sock!.profilePictureUrl(contact.id!)
+            console.log(
+              `contact ${contact.id} has a new profile pic: ${newUrl}`
+            )
+          }
+        }
+      }
 
-	return sock
+      if (events['chats.delete']) {
+        console.log('chats deleted ', events['chats.delete'])
+      }
+    }
+  )
+
+  return sock
 }
 
 startSock()
